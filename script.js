691ce49498cdbcc2fffc08e2be72bb6934be8226
Fisayomi <estherjoshua2002@gmail.com>
2022-09-22 00:23:34 +0100

    second commit

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function myNavFunction() {
         eseNav.classList.remove("sticky")
     }
 }
+const readmoreEl1 = document.getElementById("readmore1")
+
+function readmore1() {
+    readmoreEl1.innerText +=" " + "Animi recusandae odit asperiores consectetur provident maiores ad nobis aspernatur excepturi vitae nam eligendi, commodi labore autem temporibus laudantium exercitationem tempore quidem"
+    document.getElementById("btn1").style.display = "none";
+}
+
+const readmoreEl2 = document.getElementById("readmore2")
+
+function readmore2() {
+    readmoreEl2.innerText +=" " + "Animi recusandae odit asperiores consectetur provident maiores ad nobis aspernatur excepturi vitae nam eligendi, commodi labore autem temporibus laudantium exercitationem tempore quidem"
+    document.getElementById("btn2").style.display = "none";
+}
+
+const readmoreEl3 = document.getElementById("readmore3")
+
+function readmore3() {
+    readmoreEl3.innerText +=" " + "Animi recusandae odit asperiores consectetur provident maiores ad nobis aspernatur excepturi vitae nam eligendi, commodi labore autem temporibus laudantium exercitationem tempore quidem"
+    document.getElementById("btn3").style.display = "none";
+}
+
